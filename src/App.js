6ef3813a7dcf4e614f17779c9829c8977f106d07
mwong01 <mattw90@gmail.com>
2020-01-28 21:44:12 -0500

    install react icons and build Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import { Header} from './components/layout/Header';
 
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>helloooo!</p>
-      </header>
+      <Header />
     </div>
   );
 };
