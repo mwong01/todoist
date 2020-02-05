159beb809212a61749cb526680a6cedc417a180c
mwong01 <mattw90@gmail.com>
2020-02-05 15:48:27 -0500

    fix Add Project styling and bugs

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,62 +1,76 @@
-import React, { useState } from "react";
-import { firebase } from "../firebase";
-import { generatePushId } from "../helpers";
-import { useProjectsValue } from "../context";
+import React, { useState } from 'react';
+import { firebase } from '../firebase';
+import { generatePushId } from '../helpers';
+import { useProjectsValue } from '../context';
 
 export const AddProject = ({ shouldShow = false }) => {
   const [show, setShow] = useState(shouldShow);
-  const [projectName, setProjectName] = useState("");
+  const [projectName, setProjectName] = useState('');
 
   const projectId = generatePushId();
-  const { setProjects } = useProjectsValue();
+  const { projects, setProjects } = useProjectsValue();
 
   const addProject = () =>
     projectName &&
-      firebase
-        .firestore()
-        .collection("projects")
-        .add({
-          projectId,
-          name: projectName,
-          userId: "test"
-        })
-        .then(() => {
-          setProjects([]);
-          setProjectName("");
-          setShow(false);
-        });
-    return (
-      <div className="add-project" data-testid="add-project">
-        {show && (
-          <div className="add-project__input">
-            <input
-              value={projectName}
-              onChange={e => setProjectName(e.target.value)}
-              className="add-project__name"
-              data-testid="project-name"
-              type="text"
-              placenholder="Name your project"
-            />
-            <button
-              className="add-project__submit"
-              type="button"
-              onClick={() => addProject()}
-              data-testid="add-project-submit"
-            />
-            <span
-              data-testid="hide-project-overlay"
-              className="add-project__cancel"
-              onClick={() => setShow(false)}
-            />
+    firebase
+      .firestore()
+      .collection('projects')
+      .add({
+        projectId,
+        name: projectName,
+        userId: 'jlIFXIwyAL3tzHMtzRbw',
+      })
+      .then(() => {
+        setProjects([...projects]);
+        setProjectName('');
+        setShow(false);
+      });
+
+  return (
+    <div className="add-project" data-testid="add-project">
+      {show && (
+        <div className="add-project__input" data-testid="add-project-inner">
+          <input
+            value={projectName}
+            onChange={e => setProjectName(e.target.value)}
+            className="add-project__name"
+            data-testid="project-name"
+            type="text"
+            placeholder="Name your project"
+          />
+          <button
+            className="add-project__submit"
+            type="button"
+            onClick={() => addProject()}
+            data-testid="add-project-submit"
+          >
+            Add Project
+          </button>
+          <span
+            aria-label="Cancel adding project"
+            data-testid="hide-project-overlay"
+            className="add-project__cancel"
+            onClick={() => setShow(false)}
+            onKeyDown={() => setShow(false)}
+            role="button"
+            tabIndex={0}
+          >
             Cancel
-          </div>
-        )}
-        <span className='add-project__plus'>+</span>
-        <span
-        data-testid='add-project-action'
-        className='add-project__test'
-        onClick={()=> setShow(!show)}
-        >Add Project</span>
-      </div>
-    );
-  };
+          </span>
+        </div>
+      )}
+      <span className="add-project__plus">+</span>
+      <span
+        aria-label="Add Project"
+        data-testid="add-project-action"
+        className="add-project__text"
+        onClick={() => setShow(!show)}
+        onKeyDown={() => setShow(!show)}
+        role="button"
+        tabIndex={0}
+      >
+        Add Project
+      </span>
+    </div>
+  );
+};
