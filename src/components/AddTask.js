e48260d7c7febae2d8e2d77d1c566e4ca7c928f5
mwong01 <mattw90@gmail.com>
2020-02-06 17:11:45 -0500

    add AddTask component

---
+++ src/components/AddTask.js
@@ -0,0 +1,6 @@
+import React, {useState} from 'react';
+import {FaRegListAlt, FaRegCalendarAlt} from 'react-icons/fa;'
+import moment from 'moment';
+import {firebase} from '../firebase';
+import {useSelectedProjectValue} from '../context';
+
